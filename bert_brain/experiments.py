a6bcda3fe42f2fd9e3079e6e8971b35dc00ba821
onlybranch <onlybranch@proton.me>
2023-08-23 18:42:51 +0000

    Revert "drm/radeon: fix bo creation retry path"

--- bert_brain/experiments.py
+++ bert_brain/experiments.py
@@ -8,4 +8,30 @@ import torch
 from .common import SwitchRemember
 from .data_sets import ResponseKind, CorpusTypes, PreprocessDetrend, PreprocessStandardize
 from .modeling import KeyedLinear
-from .settings import TrainingV
+from .settings import TrainingVariation, LoadFrom, Settings, OptimizationSettings, PredictionHeadSettings
+
+__all__ = ['task_hash', 'set_random_seeds', 'iterate_powerset', 'named_variations', 'match_variation']
+
+
+def _internal_hash_update(hash_, loss_tasks):
+    if isinstance(loss_tasks, TrainingVariation):
+        for loss_task in sorted(loss_tasks.loss_tasks):
+            hash_.update(loss_task.encode())
+        if loss_tasks.load_from is not None:
+            hash_.update(loss_tasks.load_from.variation_name.encode())
+            _internal_hash_update(hash_, loss_tasks.load_from.loss_tasks)
+    else:
+        for loss_task in sorted(loss_tasks):
+            hash_.update(loss_task.encode())
+
+
+def task_hash(loss_tasks):
+    hash_ = hashlib.sha256()
+    _internal_hash_update(hash_, loss_tasks)
+    return hash_.hexdigest()
+
+
+def set_random_seeds(seed, index_run, n_gpu):
+    hash_ = hashlib.sha256('{}'.format(seed).encode())
+    hash_.update('{}'.format(index_run).encode())
+   
