772e69c0523f0b5670ae6d521a09265bf4824679
onlybranch <onlybranch@proton.me>
2023-08-29 19:31:33 +0000

    store

--- bert_brain/modeling/utility_modules.py
+++ bert_brain/modeling/utility_modules.py
@@ -19,4 +19,31 @@ def at_most_one_data_id(data_ids, return_first_index=False, return_last_index=Fa
 
     if return_first_index or return_last_index:
         index_array = torch.arange(data_ids.size()[1], device=data_ids.device).view(
-            (1, data_ids.size()[1])).repeat((data_ids
+            (1, data_ids.size()[1])).repeat((data_ids.size()[0], 1))
+        indicator_valid = data_ids >= 0
+        first_index = None
+        if return_first_index:
+            first_index_array = torch.where(
+                indicator_valid, index_array, torch.full_like(index_array, data_ids.size()[1] + 1))
+            first_index, _ = torch.min(first_index_array, dim=1)
+        last_index = None
+        if return_last_index:
+            last_index_array = torch.where(indicator_valid, index_array, torch.full_like(index_array, -1))
+            last_index, _ = torch.max(last_index_array, dim=1)
+        if return_first_index and return_last_index:
+            return maxes, first_index, last_index
+        if return_first_index:
+            return maxes, first_index
+        if return_last_index:
+            return maxes, last_index
+
+    return maxes
+
+
+def k_data_ids(k, data_ids, return_indices=False, check_unique=False):
+
+    if len(data_ids.size()) != 2:
+        raise ValueError('data_ids must be 2D')
+
+    indicator_valid = data_ids >= 0
+    count_valid = torch.sum(indicat
